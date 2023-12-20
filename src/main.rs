60e53e2348a81b6a55ee3f45457945ab4b8a3881
Nick Stambaugh <nastambaugh@gmail.com>
2023-12-19 22:34:37 -0500

    test passed, parsed well, ready for beta release

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|_err| {
+        eprintln!("Problem parsing arguments: {_err}");
         process::exit(1);
     });
 
     if let Err(_e) = nsgrep::run(config) {
+        eprintln!("CRITICAL ERROR: {_e}");
         process::exit(1);
     }
 }
