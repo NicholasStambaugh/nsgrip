7f344f837c06898fdbb5d3a2b3e7bf617dcecf1a
Nick Stambaugh <nastambaugh@gmail.com>
2023-12-19 20:59:06 -0500

    starting error handling

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,35 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let(_query, _file_path) = parse_config(&args);
 
-    let query = &args[1];
-    let file_path = &args[2];
+    // Use the Config::new function to create a Config instance
+    let config = Config::new(&args);
+    
+    println!("Search for {}", config.query);
+    println!("in file... {}", config.file_path);
 
-    println!("Searching for {}", query);
-    println!("in file... {}", file_path);
+    // Use the fields directly from the config instance
+    let contents = fs::read_to_string(&config.file_path).expect("Should be able to read file!");
 
-    let contents = fs::read_to_string(file_path).expect("Should be able to read file!");
-
-    println!("With text:\n{contents}");
+    println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
-    (query,file_path)
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    // Rename the function to be more descriptive
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+
+        // Use the fields directly instead of creating a new Config instance
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
