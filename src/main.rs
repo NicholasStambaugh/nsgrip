241d3167ec7796e5c28a2a83e619e24f17ad5444
Nick Stambaugh <nastambaugh@gmail.com>
2023-12-19 21:22:30 -0500

    refactored into new file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use nsgrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // Use the Config::new function to create a Config instance
+    // Use the Config::build function to create a Config instance
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguemnt: {err}");
         process::exit(1);
@@ -15,36 +15,8 @@ fn main() {
     println!("Search for {}", config.query);
     println!("in file... {}", config.file_path);
 
-    run(config);
-
-    // Use the fields directly from the config instance
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(&config.file_path).expect("Should be able to read file!");
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    
-
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        // Use the fields directly instead of creating a new Config instance
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = nsgrep::run(config) {
+        println!("CRITICAL ERROR: {e}");
+        process::exit(1);
     }
 }
